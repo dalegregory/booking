0a41c9cf101fe8e360d76a994f41f5d0e93cba36
Oliver Delevingne <oliver@delevingne.co.uk>
2015-01-04 20:09:57 +0000

    it can check that the booking is for a single row

--- lib/booking_request.rb
+++ lib/booking_request.rb
@@ -12,4 +12,8 @@ class BookingRequest
     @booking_info[:id] >= 0
   end
 
+  def single_row?
+    @booking_info[:startrow] == @booking_info[:endrow]
+  end
+
 end
--- spec/booking_request_spec.rb
+++ spec/booking_request_spec.rb
@@ -10,10 +10,16 @@ describe 'Booking Request' do
     expect(missing_fields.necessary_fields?).to be false
   end
 
-  it 'has a valid id of 0 or above' do
+  it 'can check if it has a valid id of 0 or above' do
     invalid_id = BookingRequest.new({ :id => -1, :startrow => 77, :firstseat => 23, :endrow => 77, :lastseat => 24 })
     expect(valid_request.valid_id?).to be true
     expect(invalid_id.valid_id?).to be false
+  end
+
+  it 'can check if the booking is for a single row' do
+    split_rows = BookingRequest.new({ :id => 1, :startrow => 77, :firstseat => 23, :endrow => 78, :lastseat => 24 })
+    expect(split_rows.single_row?).to be false
+    expect(valid_request.single_row?).to be true
   end
 
 end
