13c986f23779b7951f415a8d0bb8addcef42df90
Oliver Delevingne <oliver@delevingne.co.uk>
2015-01-04 19:50:49 +0000

    BookingRequest can check if the :id given is valid

--- lib/booking_request.rb
+++ lib/booking_request.rb
@@ -8,4 +8,8 @@ class BookingRequest
     @booking_info.size == 5
   end
 
+  def valid_id?
+    @booking_info[:id] >= 0
+  end
+
 end
--- spec/booking_request_spec.rb
+++ spec/booking_request_spec.rb
@@ -2,11 +2,18 @@ require 'booking_request'
 
 describe 'Booking Request' do
 
+  let(:valid_request) { BookingRequest.new({ :id => 0, :startrow => 77, :firstseat => 23, :endrow => 77, :lastseat => 24 }) }
+
   it 'is initialized with 5 key value pairs' do
-    valid_request = BookingRequest.new({ :id => 0, :startrow => 77, :firstseat => 23, :endrow => 77, :lastseat => 24 })
-    missing_fields = BookingRequest.new({ :id => 0, :startrow => 77, :firstseat => 23, :endrow => 77})
+    missing_fields = BookingRequest.new({ :id => 0, :startrow => 77, :firstseat => 23, :endrow => 77 })
     expect(valid_request.necessary_fields?).to be true
     expect(missing_fields.necessary_fields?).to be false
+  end
+
+  it 'has a valid id of 0 or above' do
+    invalid_id = BookingRequest.new({ :id => -1, :startrow => 77, :firstseat => 23, :endrow => 77, :lastseat => 24 })
+    expect(valid_request.valid_id?).to be true
+    expect(invalid_id.valid_id?).to be false
   end
 
 end
